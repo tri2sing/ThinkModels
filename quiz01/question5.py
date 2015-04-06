06a157be5010868cda7e2fba3d8119402f505175
tri2sing <tri2sing@yahoo.com>
2015-04-06 08:40:53 -0700

    Refactored quiz 1 question 5 part 1 to handle part 2

--- quiz01/question5.py
+++ quiz01/question5.py
@@ -6,13 +6,19 @@ Created on Apr 6, 2015
 
 import random
 
-def sampleQuizzes():
-    numTrials = 10000
+def generateScores(numTrials):
+    """
+    Runs numTrials trials of score-generation for each of
+    three exams (Midterm 1, Midterm 2, and Final Exam).
+    Generates uniformly distributed scores for each of 
+    the three exams, then calculates the final score and
+    appends it to a list of scores.
+    
+    Returns: A list of numTrials scores.
+    """
     weightMid1 = 0.25
     weightMid2 = 0.25
     weightFinal = 0.5
-    lowerBound = 70
-    upperBound = 75
     scores = [0] * numTrials
     
     for i in range(numTrials):
@@ -21,9 +27,20 @@ def sampleQuizzes():
         finalExam = random.randint(55, 95)
         scores[i] = weightMid1 * midTerm1 + weightMid2 * midTerm2 + weightFinal * finalExam
     
+    return scores
+
+
+def sampleQuizzes():
+    """    
+    Returns: The probability that the scores lie between [70, 75]
+    """
+    lowerBound = 70
+    upperBound = 75
+    scores = generateScores(10000)
     subset = [score for score in scores if score >= lowerBound and score <= upperBound]
-    
+
     return len(subset)/float(len(scores))
+    
     
 if __name__ == '__main__':
     print 'Probability = ', sampleQuizzes()
