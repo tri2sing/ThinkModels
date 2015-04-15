44d2e491f08365b8c0c98cce86d4c0a4bba8785c
tri2sing <tri2sing@yahoo.com>
2015-04-15 10:02:24 -0700

    Added code regime for two drug simulation

--- problemset04/ps4.py
+++ problemset04/ps4.py
@@ -6,7 +6,7 @@ import pylab
 from ps3c import *
 # from problemset03.ps3b import *
 
-def singleTrial(numViruses=100, maxPop=1000, maxBirthProb=0.1, clearProb=0.05,
+def trialOnePrescription(numViruses=100, maxPop=1000, maxBirthProb=0.1, clearProb=0.05,
                 mutProb=0.005, stepsBeforeDrug=0, stepsAfterDrug=150):
     '''
     '''
@@ -67,7 +67,7 @@ def simulationDelayedTreatment(numTrials):
     for k in range(len(stepsBefore)):
         totals = [0] * numTrials
         for i in range(numTrials):
-            totals[i] = singleTrial(stepsBeforeDrug=stepsBefore[k])
+            totals[i] = trialOnePrescription(stepsBeforeDrug=stepsBefore[k])
         print 'Done with steps before treatment = ' + str(stepsBefore[k])
         pylab.subplot(2, 2, k + 1)
         pylab.title('Steps before = ' + str(stepsBefore[k]))
@@ -96,7 +96,7 @@ def simulationVirusListLength(numTrials):
     for k in range(len(virusListLength)):
         totals = [0] * numTrials
         for i in range(numTrials):
-            totals[i] = singleTrial(numViruses=virusListLength[k], stepsBeforeDrug=150)
+            totals[i] = trialOnePrescription(numViruses=virusListLength[k], stepsBeforeDrug=150)
         print 'Done with virus list length = ' + str(virusListLength[k])
         pylab.subplot(2, 2, k + 1)
         pylab.title('Virus List Length = ' + str(virusListLength[k]))
@@ -124,7 +124,7 @@ def simulationMaxPopCount(numTrials):
     for k in range(len(maxPop)):
         totals = [0] * numTrials
         for i in range(numTrials):
-            totals[i] = singleTrial(maxPop=maxPop[k], stepsBeforeDrug=150)
+            totals[i] = trialOnePrescription(maxPop=maxPop[k], stepsBeforeDrug=150)
         print 'Done with max pop count = ' + str(maxPop[k])
         pylab.subplot(2, 2, k + 1)
         pylab.title('Max Pop Count = ' + str(maxPop[k]))
@@ -152,7 +152,7 @@ def simulationMaxBirthProb(numTrials):
     for k in range(len(maxBirthProb)):
         totals = [0] * numTrials
         for i in range(numTrials):
-            totals[i] = singleTrial(maxBirthProb=maxBirthProb[k], stepsBeforeDrug=150)
+            totals[i] = trialOnePrescription(maxBirthProb=maxBirthProb[k], stepsBeforeDrug=150)
         print 'Done with max birth prob = ' + str(maxBirthProb[k])
         pylab.subplot(2, 2, k + 1)
         pylab.title('Max Birth Prob = ' + str(maxBirthProb[k]))
@@ -180,7 +180,7 @@ def simulationClearProb(numTrials):
     for k in range(len(clearProb)):
         totals = [0] * numTrials
         for i in range(numTrials):
-            totals[i] = singleTrial(clearProb=clearProb[k], stepsBeforeDrug=150)
+            totals[i] = trialOnePrescription(clearProb=clearProb[k], stepsBeforeDrug=150)
         print 'Done with clear prob = ' + str(clearProb[k])
         pylab.subplot(2, 2, k + 1)
         pylab.title('Clear Prob = ' + str(clearProb[k]))
@@ -207,7 +207,7 @@ def simulationGivenFactor(numTrials=0, stepsBeforeDrug=150, factorName='', facto
             args = {}
             args[str(factorName)] = factorValues[k]
             args['stepsBeforeDrug'] = stepsBeforeDrug
-            totals[i] = singleTrial(**args)
+            totals[i] = trialOnePrescription(**args)
         print 'Done with'  + factorName + ' = ' + str(factorValues[k])
         pylab.subplot(numValues / 2, numValues - (numValues / 2), k + 1)
         pylab.title(factorName + ' = ' + str(factorValues[k]))
@@ -216,10 +216,30 @@ def simulationGivenFactor(numTrials=0, stepsBeforeDrug=150, factorName='', facto
     pylab.show()   
     
 
-simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='numViruses', factorValues=[50, 100, 150, 200])
-simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='maxPop', factorValues=[1000, 1500, 2000, 2500])
-simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='maxBirthProb', factorValues=[0.1, 0.2, 0.3, 0.4])
-simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='clearProb', factorValues=[0.05, 0.1, 0.15, 0.2])
+#simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='numViruses', factorValues=[50, 100, 150, 200])
+#simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='maxPop', factorValues=[1000, 1500, 2000, 2500])
+#simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='maxBirthProb', factorValues=[0.1, 0.2, 0.3, 0.4])
+#simulationGivenFactor(numTrials=10, stepsBeforeDrug=150, factorName='clearProb', factorValues=[0.05, 0.1, 0.15, 0.2])
+
+
+def trialTwoPrescriptions(numViruses=100, maxPop=1000, maxBirthProb=0.1, clearProb=0.05, mutProb=0.005, 
+                          stepsBeforeFirstDrug=150, stepsBetweenTwoDrugs=0, stepsAfterSecondDrug=150,
+                          ):
+    '''
+    '''
+    resistances = {'guttagonol': False, 'grimpex': False}
+    totalViruses = 0 
+    viruses = [ResistantVirus(maxBirthProb, clearProb, resistances, mutProb) for m in range(numViruses)]
+    patient = TreatedPatient(viruses, maxPop)
+    for _ in range(stepsBeforeFirstDrug):
+        totalViruses = patient.update()
+    patient.addPrescription('guttagonol')
+    for _ in range(stepsBeforeFirstDrug, stepsBeforeFirstDrug + stepsBetweenTwoDrugs):
+        totalViruses = patient.update()
+    patient.addPrescription('grimpex')
+    for _ in range(stepsBeforeFirstDrug + stepsBetweenTwoDrugs, stepsBeforeFirstDrug + stepsBetweenTwoDrugs + stepsAfterSecondDrug):
+        totalViruses = patient.update()
+    return totalViruses
 
 #
 # PROBLEM 2
@@ -237,4 +257,18 @@ def simulationTwoDrugsDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
-    # TODO
+    pylab.figure()
+    stepsBetweenTwoDrugs = [300, 150, 75, 0]
+    numValues = len(stepsBetweenTwoDrugs)
+    for k in range(numValues):
+        totals = [0] * numTrials
+        for i in range(numTrials):
+            totals[i] = trialTwoPrescriptions(stepsBetweenTwoDrugs=stepsBetweenTwoDrugs[k])
+        print 'Done with steps between treatments = ' + str(stepsBetweenTwoDrugs[k])
+        pylab.subplot(numValues / 2, numValues - (numValues / 2), k + 1)
+        pylab.title('Steps between = ' + str(stepsBetweenTwoDrugs[k]))
+        pylab.ylabel('Number of Trials')
+        pylab.hist(totals, 11)
+    pylab.show()   
+
+simulationTwoDrugsDelayedTreatment(250)
