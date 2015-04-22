25a33c33d62188183bee2974d1c3346e61dee39b
tri2sing <tri2sing@yahoo.com>
2015-04-21 18:30:54 -0700

    Added the sample DFS and BFS code given in lecture

---
+++ lecture09/traversals.py
@@ -0,0 +1,84 @@
+'''
+Created on Apr 21, 2015
+
+@author: Sameer Adhikari
+'''
+
+from lecture09.graph import *
+
+def DFS(graph, start, end, path = [], shortest = None):
+    #assumes graph is a Digraph
+    #assumes start and end are nodes in graph
+    path = path + [start]
+    print 'Current dfs path:', printPath(path)
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path: #avoid cycles
+            newPath = DFS(graph,node,end,path,shortest)
+            if newPath != None:
+                return newPath
+
+
+def DFSShortest(graph, start, end, path = [], shortest = None):
+    #assumes graph is a Digraph
+    #assumes start and end are nodes in graph
+    path = path + [start]
+    print 'Current dfs path:', printPath(path)
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path: #avoid cycles
+            if shortest == None or len(path)<len(shortest):
+                newPath = DFSShortest(graph,node,end,path,shortest)
+                if newPath != None:
+                    shortest = newPath
+    return shortest
+
+def BFS(graph, start, end, q = []):
+    initPath = [start]
+    q.append(initPath)
+    while len(q) != 0:
+        tmpPath = q.pop(0)
+        lastNode = tmpPath[len(tmpPath) - 1]
+        print 'Current dequeued path:', printPath(tmpPath)
+        if lastNode == end:
+            return tmpPath
+        for linkNode in graph.childrenOf(lastNode):
+            if linkNode not in tmpPath:
+                newPath = tmpPath + [linkNode]
+                q.append(newPath)
+    return None
+
+def test():
+    nodes = []
+    for name in range(6):
+        nodes.append(Node(str(name)))
+    g = Digraph()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[2]))
+    g.addEdge(Edge(nodes[2],nodes[3]))
+    g.addEdge(Edge(nodes[2],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[5]))
+    g.addEdge(Edge(nodes[0],nodes[2]))
+    g.addEdge(Edge(nodes[1],nodes[0]))
+    g.addEdge(Edge(nodes[3],nodes[1]))
+    g.addEdge(Edge(nodes[4],nodes[0]))
+    pathDFS = DFS(g, nodes[0], nodes[5])
+    print 'Path found by DFS:', printPath(pathDFS)
+    print
+    shortest = DFSShortest(g, nodes[0], nodes[5])
+    print 'Path found by DFS shortest:', printPath(shortest)
+    print
+    pathBFS = BFS(g, nodes[0], nodes[5])
+    print 'Path found by BFS:', printPath(pathBFS)
+
+    
+test()
+
+
+    
+    
